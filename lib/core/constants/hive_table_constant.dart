f5548666d97c1861d5fe69015ed3d8bbcad0e0de
Bhusan Shrestha <bhusansht@gmail.com>
2026-01-02 21:07:37 +0545

    code updated

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,12 @@
+class HiveTableConstant {
+  // Private constructor
+  HiveTableConstant._();
+
+  // Database name
+  static const String dbName = "smart_health_care_db";
+
+  // Tables -> Box : Index
+  static const int authTypeId = 0;
+  static const String authTable = "auth_table";
+
+}
