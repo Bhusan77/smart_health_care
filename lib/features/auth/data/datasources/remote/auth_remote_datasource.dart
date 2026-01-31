a1da715cc34f0d51fe963ab850539f63a92e9f36
Bhusan Shrestha <bhusansht@gmail.com>
2026-01-31 20:31:17 +0545

    code update done

--- lib/features/auth/data/datasources/remote/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/remote/auth_remote_datasource.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:smart_health_care/core/api/api_client.dart';
 import 'package:smart_health_care/core/api/api_endpoints.dart';
+import 'package:smart_health_care/core/services/storage/token_service.dart';
 import 'package:smart_health_care/core/services/storage/user_session_service.dart';
 import 'package:smart_health_care/features/auth/data/datasources/auth_datasource.dart';
 import 'package:smart_health_care/features/auth/data/models/auth_api_model.dart';
-import 'package:smart_health_care/features/auth/data/models/auth_hive_model.dart';
 
 
 // Create provider
@@ -12,20 +12,24 @@ final authRemoteDatasourceProvider = Provider<IAuthRemoteDataSource>((ref) {
   return AuthRemoteDatasource(
     apiClient: ref.read(apiClientProvider),
     userSessionService: ref.read(userSessionServiceProvider),
+    tokenSessionService: ref.read(tokenServiceProvider),
   );
 });
-
 
 
 class AuthRemoteDatasource implements IAuthRemoteDataSource {
   final ApiClient _apiClient;
   final UserSessionService _userSessionService;
 
+final TokenService _tokenSessionService; 
   AuthRemoteDatasource({
     required ApiClient apiClient,
     required UserSessionService userSessionService,
+     required TokenService tokenSessionService,
   }) : _apiClient = apiClient,
-       _userSessionService = userSessionService;
+       _userSessionService = userSessionService,
+       _tokenSessionService=tokenSessionService;
+
 
   
 
@@ -40,12 +44,12 @@ class AuthRemoteDatasource implements IAuthRemoteDataSource {
       final data = response.data['data'] as Map<String, dynamic>;
       final user = AuthApiModel.fromJson(data);
 
-      // Save to session
-      // await _userSessionService.saveUserSession(
-      //   userId: user.id!,
-      //   email: user.email,
-      //   username: user.username!,
-      // );
+    final token = response.data['token'] as String;
+    await _tokenSessionService.saveToken(token);
+    print("my user id: ${user.id}");
+      await _userSessionService.saveUserSession(
+        userId: user.id,
+      );
       return user;
     }
 
